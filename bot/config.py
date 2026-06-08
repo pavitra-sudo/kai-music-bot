04e8141c57e85ff996c52cb2aefeb476485a184d
Pavitra N Dubey <213853274+pavitra-sudo@users.noreply.github.com>
2026-06-08 17:22:06 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,14 @@
-from dotenv import load_dotenv
 import os
-load_dotenv()
+from pathlib import Path
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("BOT_TOKENV")
-DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH", "downloads")
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+
+load_dotenv(dotenv_path=ENV_PATH)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH", "/mnt/music/music")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found (check .env loading)")
